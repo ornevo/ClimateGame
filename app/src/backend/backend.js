a25dccd4ae95822522361b82e508c658414db6f2
TheGoldman <eliav852@gmail.com>
2022-07-26 12:30:28 +0300

    combining zamir and Eliav

--- app/src/backend/backend.js
+++ app/src/backend/backend.js
@@ -1,8 +1,17 @@
 import db from "../components/db.json";
-
-var state = {
-
-}
+import {INITIAL_EMISSIONS, INITIAL_MONEY, INIITIAL_QUALITY_OF_LIFE,
+    INITIAL_YEAR}
+     from '../constants'
+     state = {
+        active_events: [],
+        deleted_events: [],
+        options: [],
+        effects: [],
+        emmisions: INITIAL_EMISSIONS,
+        money: INITIAL_MONEY,
+        quality_of_life: INIITIAL_QUALITY_OF_LIFE,
+    
+    }
 
 
 
@@ -16,13 +25,15 @@ function getUpdatedState()
 
 }
 
-function deleteEvent(eventId)
-{
+function deleteEvent(eventId){
+    this.state.active_events.filter(function(e) {return e!=eventId});
+    this.state.deleted_events.push(eventId);
 
 }
+
 export default {
     state,
     updateStateByOption,
     getUpdatedState,
     deleteEvent
-}
+}       

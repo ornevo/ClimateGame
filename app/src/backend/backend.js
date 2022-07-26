d5011abd60b3e058ac2ef549848269f4a2e54309
rons1 <ron.shmuter@gmail.com>
2022-07-26 12:27:09 +0300

    Backend wip

---
+++ app/src/backend/backend.js
@@ -0,0 +1,28 @@
+import db from "../components/db.json";
+
+var state = {
+
+}
+
+
+
+function updateStateByOption(chosenOptionId)
+{
+
+}
+
+function getUpdatedState()
+{
+
+}
+
+function deleteEvent(eventId)
+{
+
+}
+export default {
+    state,
+    updateStateByOption,
+    getUpdatedState,
+    deleteEvent
+}

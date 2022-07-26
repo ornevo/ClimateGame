48d43a549354dc3a59fef75ca060a1f7cb49576f
Kiran Kabaso <kirankabaso@gmail.com>
2022-07-27 02:43:27 +0300

    game instruction

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function About() {
     return (
         <section className="about-container">
-            <h1>הוראות משחק</h1>
+            <h1 className="about-title">הוראות משחק</h1>
+            <hr></hr>
+            <h3 className="about-explain">האנושות ממשיכה לפלוט עוד ועוד גזי חממה, המשפיעים על תנאי החיים שלנו בכדור הארץ.
+
+במהלך המשחק, מטרתך היא לדאוג לתושביך ולעתיד הדורות הבאים.
+
+בידך לבחור איך תגיע/י לשם, מ2022-2050</h3>
+            <h1 className="about-title">החוקים</h1>
+            <hr></hr>
+            <h3 className="about-explain">איפוס סף הפליטות ועלייה באיכות החיים במסגרת התקציב שלך תעלה את סיכוייך לזכות. 
+
+הגעה לקצה אחד משלושת המדדים במשחק תגרור פסילה</h3>
+            <hr></hr>
+            <h1 className="about-title">אנשיך סומכים עליך, בהצלחה!</h1>
         </section>
     )
 }
